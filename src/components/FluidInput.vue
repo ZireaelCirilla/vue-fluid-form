b7c98bc862ac059d78f935851a663422dad2395f
Marce Graffigna <marcesmartphone@gmail.com>
2019-08-14 19:49:45 +0100

    first commit

---
+++ src/components/FluidInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="fluid-input"></div>
+</template>
+
+<script>
+export default {
+  name: "FluidInput",
+  props: ["inputIntructions"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
